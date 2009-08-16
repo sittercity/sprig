7456d8f571242c1c890f000f0922435a5cbe0f83
Woody Gilk <woody.gilk@kohanaphp.com>
2009-08-15 21:32:34 -0500

    Added Sprig_Field_Email

---
+++ classes/sprig/field/email.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Sprig_Field_Email extends Sprig_Field_Char {
+
+	public $rules = array('email' => NULL);
+
+} // End Sprig_Field_Email
