b31567828be36ad39bcf80072029810c32959fb0
Kelvin Luck <kelvinluck@k2000.home>
2009-09-13 20:58:14 +0800

    Added docs and more resize types to image field type...

--- classes/sprig/field/image.php
+++ classes/sprig/field/image.php
@@ -2,12 +2,45 @@
 
 class Sprig_Field_Image extends Sprig_Field_Char 
 {
-	const RESIZE_TYPE_CROP = 'RESIZE_TYPE_CROP';
-	const RESIZE_TYPE_FIT = 'RESIZE_TYPE_FIT';
 	
+	/**
+	 * @const string Image will be cropped to the passed size (via Image->crop);
+	 */
+	const RESIZE_TYPE_CROP = 'RESIZE_TYPE_CROP';
+	/**
+	 * @const string Image will be resized to fit in the passed size (via Image->resize);
+	 */
+	const RESIZE_TYPE_FIT = 'RESIZE_TYPE_FIT';
+	/**
+	 * @const string Image will be resized and cropped to fit exactly in the passed size
+	 * while retaining as much of the image as possible (via Image->resizeAndCrop);
+	 */
+	const RESIZE_TYPE_EXACT_FIT = 'RESIZE_TYPE_EXACT_FIT';
+	/**
+	 * @const string Image will not be cropped or resized at all and an Exception will
+	 * be thrown if the uploaded image isn't the correct size.
+	 */
+	const RESIZE_TYPE_EXACT = 'RESIZE_TYPE_EXACT';
+	/**
+	 * @const string Image will not be cropped or resized at all
+	 */
+	const RESIZE_TYPE_NONE = 'RESIZE_TYPE_NONE';
+	
+	/**
+	 * @var  integer  image width
+	 */
 	public $width;
+	/**
+	 * @var  integer  image height
+	 */
 	public $height;
+	/**
+	 * @var  string  path where the image will be saved to/ loaded from
+	 */
 	public $path;
+	/**
+	 * @var  string one of the RESIZE_TYPE_* constants
+	 */
 	public $resize_type;
 
 	public function input($name, array $attr = array())
