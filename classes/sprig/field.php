c9c37ea71bc5bc31613a8840ad7466e7edc868c6
Jeff Fendley <jeff.fendley@gmail.com>
2009-11-16 01:52:42 +0800

    Added Sprig_Field public variable documentation inline. Ported from readme.

--- classes/sprig/field.php
+++ classes/sprig/field.php
@@ -9,32 +9,91 @@
  */
 abstract class Sprig_Field {
 
+    /**
+     * Allow `empty()` values to be used. Default is `FALSE`.
+     * @var bool
+     */
 	public $empty = FALSE;
 
+    /**
+     * A primary key field. Multiple primary keys (composite key) can be specified. Default is `FALSE`.
+     * @var bool
+     */
 	public $primary = FALSE;
 
+    /**
+     * This field must have a unique value within the model table. Default is `FALSE`.
+     * @var bool
+     */
 	public $unique = FALSE;
 
+    /**
+     * Convert all `empty()` values to `NULL`. Default is `FALSE`.
+     * @var bool
+     */
 	public $null = FALSE;
 
+    /**
+     * Show the field in forms. Default is `TRUE`.
+     * @var bool
+     */
 	public $editable = TRUE;
 
+    /**
+     * Default value for this field. Default is `''` (an empty string).
+     * @var string
+     */
 	public $default = '';
 
+    /**
+     * Limit the value of this field to an array of choices. This will change the form input into a select list. No default value.
+     * @var array
+     */
 	public $choices;
 
+    /**
+     * Database column name for this field. Default will be the same as the field name,
+     * except for foreign keys, which will use the field name with `_id` appended.
+     * In the case of HasMany fields, this value is the column name that contains the
+     * foreign key value.
+     * @var string
+     */
 	public $column;
 
+    /**
+     * Human readable label. Default will be the field name converted with `Inflector::humanize()`.
+     * @var string
+     */
 	public $label;
 
+    /**
+     * Description of the field. Default is `''` (an empty string).
+     * @var string
+     */
 	public $description = '';
 
+    /**
+     * If true, the column is present in the database table. Default: TRUE
+     * @var bool
+     */
 	public $in_db = TRUE;
 
+    /**
+     * {@link Kohana_Validate} filters for this field.
+     * @var array
+     */
 	public $filters = array();
 
+    /**
+     * {@link Kohana_Validate} rules for this field.
+     * @var array
+     */
 	public $rules = array();
 
+    /**
+     * {@link Kohana_Validate} callbacks for this field.
+     * @var array
+     */
 	public $callbacks = array();
 
 	public function __construct(array $options = NULL)
