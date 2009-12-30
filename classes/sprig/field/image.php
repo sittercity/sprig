eaad0a542eabb06dfa88fafd76999733e94c7194
Woody Gilk <woody.gilk@kohanaphp.com>
2009-12-30 15:42:45 -0600

    Better handling of resize types in Image fields

--- classes/sprig/field/image.php
+++ classes/sprig/field/image.php
@@ -11,33 +11,6 @@
 class Sprig_Field_Image extends Sprig_Field_Char {
 
 	/**
-	 * @const string Image will be cropped to the passed size (via Image->crop);
-	 */
-	const RESIZE_TYPE_CROP = 'RESIZE_TYPE_CROP';
-
-	/**
-	 * @const string Image will be resized to fit in the passed size (via Image->resize);
-	 */
-	const RESIZE_TYPE_FIT = 'RESIZE_TYPE_FIT';
-
-	/**
-	 * @const string Image will be resized and cropped to fit exactly in the passed size
-	 * while retaining as much of the image as possible (via Image->resizeAndCrop);
-	 */
-	const RESIZE_TYPE_EXACT_FIT = 'RESIZE_TYPE_EXACT_FIT';
-
-	/**
-	 * @const string Image will not be cropped or resized at all and an Exception will
-	 * be thrown if the uploaded image isn't the correct size.
-	 */
-	const RESIZE_TYPE_EXACT = 'RESIZE_TYPE_EXACT';
-
-	/**
-	 * @const string Image will not be cropped or resized at all
-	 */
-	const RESIZE_TYPE_NONE = 'RESIZE_TYPE_NONE';
-
-	/**
 	 * @var  integer  image width
 	 */
 	public $width;
@@ -53,9 +26,9 @@ class Sprig_Field_Image extends Sprig_Field_Char {
 	public $path;
 
 	/**
-	 * @var  string one of the RESIZE_TYPE_* constants
+	 * @var  integer  one of the Image resize constants
 	 */
-	public $resize_type;
+	public $resize = Image::AUTO;
 
 	public function __construct(array $options = NULL)
 	{
