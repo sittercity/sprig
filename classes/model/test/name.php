0680ea48fa89f5e1f822860ee961b9438c3ab5fe
Stephen George <sgit-turtle.testudo12@wronghead.com>
2010-06-23 09:56:18 -0500

    Fixing the Test_Name->Test_User relation

--- classes/model/test/name.php
+++ classes/model/test/name.php
@@ -5,8 +5,7 @@ class Model_Test_Name extends Sprig {
 	protected function _init()
 	{
 		$this->_fields += array(
-			'test_user_id' => new Sprig_Field_Integer,
-			'user' => new Sprig_Field_BelongsTo(array('model'=>'Test_User')),
+			'test_user' => new Sprig_Field_BelongsTo(array('model'=>'Test_User')),
 			'name' => new Sprig_Field_Char(array('max_length' => 20)),
 		);
 	}
--- tests/sprig.php
+++ tests/sprig.php
@@ -214,7 +214,7 @@ class UnitTest_Sprig extends PHPUnit_Framework_TestCase {
 		
 		$user->name->load();
 		$this->assertEquals('one', $user->name->name);
-		$this->assertEquals(1, $user->name->test_user_id);
+		$this->assertEquals(1, $user->name->test_user->id);
 		$this->assertQueryCountIncrease(2, $q_before, $this->getQueries());
 	
 	}
@@ -227,15 +227,15 @@ class UnitTest_Sprig extends PHPUnit_Framework_TestCase {
 	{
 		$q_before = $this->getQueries();
 		$name = Sprig::factory('Test_Name');
-		$name->test_user_id = 1;
+		$name->test_user = 1;
 		$name = $name->load();
 		
 		$this->assertEquals('one', $name->name);
 		$this->assertQueryCountIncrease(1, $q_before, $this->getQueries());
 		
-		$name->user->load();
-		$this->assertEquals(1, $name->user->id);
-		$this->assertEquals('Mr', $name->user->title);
+		$name->test_user->load();
+		$this->assertEquals(1, $name->test_user->id);
+		$this->assertEquals('Mr', $name->test_user->title);
 		$this->assertQueryCountIncrease(2, $q_before, $this->getQueries());
 	}
 	
@@ -313,11 +313,11 @@ class UnitTest_Sprig extends PHPUnit_Framework_TestCase {
 		
 		$user1->name->name = 'foo';
 		$this->assertEquals('foo', $user1->name->name);
-		$this->assertEquals(2, $user1->name->test_user_id);
+		$this->assertEquals(2, $user1->name->test_user->id);
 		$this->assertQueryCountIncrease(3, $q_before, $this->getQueries());
 		
-		$user0->name->test_user_id = 12;
-		$this->assertEquals(12, $user0->name->test_user_id);
+		$user0->name->test_user = 12;
+		$this->assertEquals(12, $user0->name->test_user->id);
 		$this->assertEquals('one', $user0->name->name);
 		$this->assertQueryCountIncrease(3, $q_before, $this->getQueries());
 	}
